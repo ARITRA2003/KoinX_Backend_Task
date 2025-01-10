9028f0809a0f3d11e5050688b44757ab4c622a89
Aritra Sen <saritra460@gmail.com>
2025-01-10 23:37:57 +0530

    CrytoDataModelCreated

---
+++ Server/models/CryptoCurrencyData.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const cryptoSchema = new mongoose.Schema({
+    coin: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    marketCap: {
+        type: Number,
+        required: true
+    },
+    change24h: {
+        type: Number,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+const CryptoCurrencyDataModel = mongoose.model('CryptoCurrencyData', cryptoSchema);
+
+export default CryptoCurrencyDataModel;
