90e0bc0ae5ce03a0e662d4455dd85e34846f4d5b
Aritra Sen <saritra460@gmail.com>
2025-01-10 23:42:09 +0530

    CryptoDataFetchService created

---
+++ Server/services/FetchCryptoData.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+
+export const  fetchCryptoData = async()=>{
+  const url = process.env.COINGECKO_API;
+  console.log(url);
+  const params = {
+    vs_currency: 'usd',
+    ids: 'bitcoin,matic-network,ethereum',
+  };
+
+  try {
+    const response = await axios.get(url, { params });
+    return response.data.map(coin => ({
+      coin: coin.id,
+      price: coin.current_price,
+      marketCap: coin.market_cap,
+      change24h: coin.price_change_percentage_24h,
+    }));
+  } catch (error) {
+    console.error('Error fetching data from CoinGecko:', error);
+    return [];
+  }
+}
